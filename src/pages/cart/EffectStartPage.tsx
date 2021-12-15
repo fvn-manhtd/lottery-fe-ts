56957352f057673f3fd364fddc81eda15b4cf175
Tran Manh <tranmanh@MacBook.local>
2021-12-15 17:02:59 +0900

    fix cart

--- src/pages/cart/EffectStartPage.tsx
+++ src/pages/cart/EffectStartPage.tsx
@@ -1,10 +1,47 @@
-import { CartLayout } from "components/templates";
+import { Box, Span, Button, FlexBox } from "components/atoms";
+import { Logo } from "components/organisms";
+import { useHistory } from "react-router-dom";
+import { Route as ROUTES } from "utils";
 
 const EffectStartPage: React.FC = () => {
+  const history = useHistory();
+
   return (
-    <CartLayout>
-      <div>This is OrderComplete</div>
-    </CartLayout>
+    <Box py="1rem">
+      <Box maxWidth="320px" mx="auto" my="2rem">
+        <Logo />
+      </Box>
+
+      <FlexBox justifyContent="center" mx="auto"></FlexBox>
+
+      <FlexBox
+        justifyContent="center"
+        flexDirection="column"
+        maxWidth="480px"
+        mx="auto"
+      >
+        <Button
+          width="100%"
+          size="large"
+          color="primary"
+          variant="contained"
+          borderRadius={5}
+        >
+          <Span fontSize="1rem">スタート</Span>
+        </Button>
+
+        <Button
+          width="100%"
+          size="large"
+          color="gray"
+          variant="outlinedSecond"
+          borderRadius={5}
+          onClick={() => history.push(ROUTES.EFFECT_FINISH)}
+        >
+          <Span fontSize="1rem">スキップ</Span>
+        </Button>
+      </FlexBox>
+    </Box>
   );
 };
 
